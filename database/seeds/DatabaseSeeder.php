f9937f003161e200527bd1ecf8be19d9fe602727
Todociber <alexlaley10@gmail.com>
2016-08-06 13:00:26 -0600

    Triggers para proteger tabla de roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,16 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /*$this->call(departamentos::class);
-        $this->call(SeederRoles::class);
-        $this->call(SeederEstadoSolicitud::class);
-        $this->call(EstadoOrdenSeeder::class);
-        $this->call(TipoOrdenSeeder::class);
-        $this->call(TipoOrganizacionSeeder::class);
-        $this->call(OrganizacionSeeder::class);
-        $this->call(UsuarioSeeder::class);
-        $this->call(rolUsuario::class);
-        $this->call(SedderTipoEjecucionOrden::class);*/
+        /* $this->call(departamentos::class);
+         $this->call(SeederRoles::class);
+         $this->call(SeederEstadoSolicitud::class);
+         $this->call(EstadoOrdenSeeder::class);
+         $this->call(TipoOrdenSeeder::class);
+         $this->call(TipoOrganizacionSeeder::class);
+         $this->call(OrganizacionSeeder::class);
+         $this->call(UsuarioSeeder::class);
+         $this->call(rolUsuario::class);
+         $this->call(SedderTipoEjecucionOrden::class);
+         $this->call(TriggerProtegerRoles::class);*/
 
     }
 }
