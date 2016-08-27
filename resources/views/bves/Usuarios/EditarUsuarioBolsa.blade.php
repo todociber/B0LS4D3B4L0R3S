283581027fadf2f1c1c8f2133e06735c59238a48
pelicoalex\alexander <alpalex37176@gmail.com>
2016-08-27 11:17:11 -0600

    se edito el nuevo usuario se edito el "EditarUsuarioBolsa"

--- resources/views/bves/Usuarios/EditarUsuarioBolsa.blade.php
+++ resources/views/bves/Usuarios/EditarUsuarioBolsa.blade.php
@@ -26,21 +26,70 @@
                             <div class="content">
                                 <div class="row">
                                     <div class="col-md-12">
-                                        @include('alertas.errores')
-                                        @include('alertas.flash')
-                                        {{Form::model($usuario,['route'=>['UsuarioBolsa.update', $usuario->id],'method' =>'PUT', 'id'=>'form','role' => 'form'])  }}
-                                        @include('bves.Usuarios.FormularioUserBolsa.FormularioUserBolsa')
+                                        section class="content">
+                                        <script>
+                                            $('#usuarios').addClass('active');
+                                            $('#nuevoUsuario').addClass('active');
 
-                                    </div>
+                                        </script>
+                                        <div class="row">
+                                            <div class="col-xs-12">
+                                                <div class="row">
+                                                    <!-- left column -->
+                                                    <div class="col-md-12">
+                                                        <!-- general form elements -->
+                                                        <div class="box box-primary">
+                                                            <div class="box-header with-border">
+                                                                <h3 class="box-title">Registro de Usuario</h3>
+                                                            </div><!-- /.box-header -->
+                                                            <!-- form start -->
 
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                                {!!Form::submit('Modificar Usuario', ['class'=>'btn btn-primary btn-flat ladda-button','id'=>'btnSubmit', 'onclick'=>"waitingDialog.show('Procesando... ',{ progressType: 'info'})"])!!}
-                                <button type="button" onclick="window.location.href='{!! route('restaurarpassword',['id'=>$usuario->id]) !!}';  waitingDialog.show('Procesando... ',{ progressType: 'info'}); " class="btn btn-warning btn-flat">Restaurar la contraseña</button>
+                                                            <div class="box-body">
+                                                                <div class="content">
+                                                                    <div class="row">
+                                                                        <div class="col-md-12">
+                                                                            <form method="POST" action="http://localhost:8000/UsuarioBolsa" accept-charset="UTF-8" id="form" role="form"><input name="_token" type="hidden" value="p2Mh0xu03SINzn4WbjXpSa6ef3Tk7kDg61ARhf22">
+                                                                                <div class="form-group">
+                                                                                    <label for="Nombre">Nombre</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el nombre del usuario" name="nombre" type="text">
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <label for="Apellido">Apellido</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el apellido del usuario" name="apellido" type="text">
+                                                                                </div>
 
-                            </div>
-                            {{ Form::close() }}
+                                                                                <div class="form-group">
+                                                                                    <label for="Correo electrónico">Correo Electrónico</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el correo institucional del usuario" name="email" type="text">
+                                                                                </div>
+
+                                                                                <div class="form-group">
+                                                                                    <label for="Seleccione un estado">Seleccione Un Estado</label>
+                                                                                    <select class="form-control" required="required" id="estado" name="Estado"><option value="1">Activo</option><option value="0">Innactivo</option></select>
+                                                                                </div>
+
+
+
+
+
+
+                                                                            </form></div>
+
+                                                                    </div>
+                                                                </div>
+                                                                <div class="box-footer">
+                                                                    <input class="btn btn-primary btn-flat ladda-button" id="btnSubmit" onclick="waitingDialog.show('Procesando... ',{ progressType: 'info'})" type="submit" value="Registrar Usuario">
+                                                                </div>
+
+
+                                                            </div>
+
+                                                        </div><!-- /.box -->
+                                                    </div>
+                                                </div>
+                                            </div><!-- /.col -->
+                                        </div><!-- /.row -->
+                                        </section>
 
                         </div>
 
--- resources/views/bves/Usuarios/NuevoUsuario.blade.php
+++ resources/views/bves/Usuarios/NuevoUsuario.blade.php
@@ -20,25 +20,54 @@
                         <div class="box-header with-border">
                             <h3 class="box-title">Registro de Usuario</h3>
                         </div><!-- /.box-header -->
-                        <!-- form start -->
 
-                        <div class="box-body">
-                            <div class="content">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        @include('alertas.errores')
-                                        @include('alertas.flash')
-                                        {{Form::open(['route'=>'UsuarioBolsa.store','method' =>'POST', 'id'=>'form','role' => 'form'])  }}
-                                        @include('bves.Usuarios.FormularioUserBolsa.FormularioUserBolsa')
 
-                                    </div>
+                                                            <div class="box-body">
+                                                                <div class="content">
+                                                                    <div class="row">
+                                                                        <div class="col-md-12">
+                                                                            <form  accept-charset="UTF-8" id="form" role="form"><input name="_token" type="hidden" value="p2Mh0xu03SINzn4WbjXpSa6ef3Tk7kDg61ARhf22">
+                                                                                <div class="form-group">
+                                                                                    <label for="Nombre">Nombre</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el nombre del usuario" name="nombre" type="text">
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <label for="Apellido">Apellido</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el apellido del usuario" name="apellido" type="text">
+                                                                                </div>
 
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                                {!!Form::submit('Registrar Usuario', ['class'=>'btn btn-primary btn-flat ladda-button','id'=>'btnSubmit', 'onclick'=>"waitingDialog.show('Procesando... ',{ progressType: 'info'})"])!!}
-                            </div>
-                            {{ Form::close() }}
+                                                                                <div class="form-group">
+                                                                                    <label for="Correo electrónico">Correo Electrónico</label>
+                                                                                    <input class="form-control" placeholder="Ingresa el correo institucional del usuario" name="email" type="text">
+                                                                                </div>
+
+                                                                                <div class="form-group">
+                                                                                    <label for="Seleccione un estado">Seleccione Un Estado</label>
+                                                                                    <select class="form-control" required="required" id="estado" name="Estado"><option value="1">Activo</option><option value="0">Innactivo</option></select>
+                                                                                </div>
+
+
+
+
+
+
+                                                                            </form></div>
+
+                                                                    </div>
+                                                                </div>
+                                                                <div class="box-footer">
+                                                                    <input class="btn btn-primary btn-flat ladda-button" id="btnSubmit" type="submit" value="Registrar Usuario">
+                                                                </div>
+
+
+                                                            </div>
+
+                                                        </div><!-- /.box -->
+                                                    </div>
+                                                </div>
+                                            </div><!-- /.col -->
+                                        </div><!-- /.row -->
+                                        </section>
 
                         </div>
 
