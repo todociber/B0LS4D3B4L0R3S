ebb40a780deb64e29d68299f515493575c2285e2
todociberLaptop <alexlaley10@gmail.com>
2016-10-26 17:58:50 -0600

    despues del merge

--- app/Http/Controllers/BitacoraCasaCorredora.php
+++ app/Http/Controllers/BitacoraCasaCorredora.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BitacoraUsuario;
 use Auth;
+use DB;
 
 class BitacoraCasaCorredora extends Controller
 {
     public function index()
     {
-        $bitacora = BitacoraUsuario::where('idOrganizacion', '=', Auth::user()->idOrganizacion)->get();
-        return view('CasaCorredora.Bitacora.MostrarBitacota', compact("bitacora", $bitacora));
+
+        $bitacoras = DB::table("bitacora")
+            ->join("usuarios", "bitacora.idUsuario", "=", "usuarios.id")
+            ->where("bitacora.idOrganizacion", "=", Auth::user()->idOrganizacion)
+            ->orderBy("bitacora.created_at", "DESC")
+            ->select("usuarios.nombre", "usuarios.id", "bitacora.*")
+            ->get();
+        return view('CasaCorredora.Bitacora.MostrarBitacota', compact("bitacoras", $bitacoras));
 
     }
 }
--- resources/views/CasaCorredora/Bitacora/MostrarBitacota.blade.php
+++ resources/views/CasaCorredora/Bitacora/MostrarBitacota.blade.php
@@ -10,68 +10,76 @@
 
 @section('content')
     <script>
-        function ShowModalDesactivar(id, texto) {
-            $('#modalbody').text(texto);
-            $('#idusuario').val(id);
-            $('#desactivarUsuario').modal('show');
-        }
-        function ShowModalActivar(id, texto) {
-            $('#modalbodyR').text(texto);
-            $('#idrestaurar').val(id);
-            $('#activarUsuario').modal('show');
+        $('#bitacoras').addClass('active');
+        $('#bitacora').addClass('active');
+
+
+        function desactivarActivarCasa(nombre, activar, id, accion) {
+            $('#modalbody').text("¿Desea " + activar + "la casa " + nombre + " ?");
+            $('#idcasa').val(id);
+            $('#tipo').val(accion);
+
         }
 
     </script>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-header">
+                    @include('alertas.flash')
+                    <h3 class="box-title">Lista de casas</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                    <table id="example1" class="table table-bordered table-hover dataTable dt-responsive">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Usuario</th>
+                            <th>Descripción</th>
+                            <th>Fecha de realización</th>
 
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            <h3 class="box-title">Usuarios</h3>
-        </div><!-- /.box-header -->
-
-        <div class="box-body">
-            <div role="form">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            @include('alertas.flash')
-                            {!!link_to_route('UsuarioCasaCorredora.crear', $title = 'Crear Usuario ', $parameters = [], $attributes = ['class'=>'btn btn-success','onclick'=>"waitingDialog.show('Cargando... ',{ progressType: 'info'});setTimeout(function () {waitingDialog.hide();}, 3000);"])!!}
-                            <br><br>
-
-                            <div class="table-responsive">
-                                <table id="example1" class="table table-hover dt-responsive display nowrap"
-                                       cellspacing="0">
-                                    <thead>
-                                    <tr>
-
-                                        <th><p class="text-center">Tipo de cambio</p></th>
-                                        <th><p class="text-center">Usuario</p></th>
-                                        <th><p class="text-center">Descripcion del cambio</p></th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($bitacora as $bitacoras)
-                                        <tr>
-
-                                            <td>{{$bitacoras->tipoCambio}}</td>
-                                            <td>{{$bitacoras->usuarioN->nombre}} {{$bitacoras->usuarioN->apelllido}}</td>
-                                            <td>{{$bitacoras->descripcion}}</td>
-
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                                <div>
-                                </div>
-                            </div><!--row-->
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($bitacoras as $bitacora)
+                            <tr>
+                                <td>{{$bitacora->id}}</td>
+                                <td>{{$bitacora->nombre}}</td>
+                                <td>{{$bitacora->descripcion}}</td>
+                                <td>{{$bitacora->created_at}}</td>
+                            </tr>
+                        @endforeach
 
 
-                            <!-- /.box -->
-                        </div>
-                    </div>
+                        </tbody>
+
+                    </table>
+                </div><!-- /.box-body -->
+            </div><!-- /.box -->
+        </div>
+    </div><!-- /.col -->
+
+    <div class="modal fade" id="desactivarActivarCasa" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Mensaje</h4>
                 </div>
+                {{Form::open(['route'=>'eliminarrestaurarcasas','method' =>'POST', 'id'=>'form','role' => 'form', 'onsubmit'=>'animatedLoading()']) }}
+
+                {{ Form::hidden('id',null,['id'=>'idcasa','class'=>'form-control','required']) }}
+                {{ Form::hidden('tipo',null,['id'=>'tipo','class'=>'form-control', 'required']) }}
+                <div class="modal-body">
+                    <p id="modalbody"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Aceptar</button>
+                    <button type="button" data-dismiss="modal" class="btn btn-primary">Cancelar</button>
+                </div>
+                {{Form::close()}}
             </div>
         </div>
-
-
     </div>
 @stop
